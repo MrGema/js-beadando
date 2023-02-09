64167f79cf6a0d64d8b69e720417e315ec149572
Farkas Bence <farkas.b@ckik.hu>
2023-02-09 10:39:43 +0100

    véletlen kártyák generálása

--- jsprojekt/script.js
+++ jsprojekt/script.js
@@ -1,19 +1,130 @@
-var div = document.getElementById("divi")
-var table=document.createElement("table")
-for (let index = 0; index <6; index++) {
-    var tr=document.createElement("tr")
-    table.appendChild(tr)
-    for (let index = 0; index < 5; index++) {
-        var td=document.createElement("td")
-        var img=document.createElement("img")
-        img.src="img/"+(Math.floor(Math.random()*23)+1)+".png"
-        img.style.width="100px"
-        img.style.height="100px"
-        img.alt="KEP"
-        img.title="kep"
-        td.appendChild(img)
-        tr.appendChild(td)
+/*var objektum = {
+    szam1: 34,
+    szam2: 23,
+    id: 0,
+    ertek: "Zöldvár4",
+    Torol: Torol(),
+}
 
+function Torol(){
+    
+    return console.log("töröl");
+    
+}
+
+console.log(objektum.szam1);
+call(objektum.Torol);*/
+
+var jatekTer = document.getElementById("jatekter");
+var tabla = document.createElement("div");
+var leftSide = document.createElement("div");
+var kartyaBox = document.createElement("div");
+var pontBox = document.createElement("div");
+var korokBox = document.createElement("div");
+
+function JatekTerBetoltese(){
+    leftSide.append(pontBox);
+    leftSide.append(kartyaBox);
+    jatekTer.appendChild(leftSide);
+    jatekTer.appendChild(tabla);
+    jatekTer.appendChild(korokBox);
+
+    //tabla.innerHTML="tabla";
+    kartyaBox.innerHTML="kartya box";
+    pontBox.innerHTML="pontok";
+    korokBox.innerHTML="korok";
+
+}
+
+function JatekTerElrendezese(){
+    leftSide.id = "leftside";
+    kartyaBox.id = "kartya";
+    pontBox.id = "pontok";
+    tabla.id = "tabla";
+    korokBox.id = "korok";
+    jatekTer.id = "jatekTer"
+}
+
+function TablaGeneralasa(){
+    var k = 0;
+    for (let i = 0; i < 5; i++) {
+        var sordiv = document.createElement("div");
+        sordiv.classList += " sordiv";
+        for (let j = 0; j < 6; j++) 
+        {
+            var oszlopdiv = document.createElement("div");
+            //oszlopdiv.innerHTML = "X";
+            oszlopdiv.classList += " oszlopdiv";
+            oszlopdiv.id = k;
+            k++;
+            sordiv.appendChild(oszlopdiv);
+        }
+        tabla.appendChild(sordiv);
     }
 }
-div.appendChild(table)
+
+function KartyatTablabageneral(db){
+    //egy kartyat kivalasz es elhelyezzuk az elso negyzetbe
+    // a karty elhelyezese egy veletlen helyre
+    var kartyaszam = new Array();
+    var divid = new Array();
+    for (let index = 0; index < db; index++) {
+        var velkartyaszam = Math.floor(Math.random()*(23-1+1)+1);
+        var veletlendiv = Math.floor(Math.random()*(29-0+1)+0);
+        
+        if(!VaneBenne(kartyaszam, velkartyaszam))
+        {
+            kartyaszam.push(velkartyaszam);
+            var kep1 = document.createElement("img");
+            kep1.src = "img/"+velkartyaszam+".png";
+        }
+        if (!VaneBenne(divid, veletlendiv)) 
+        {
+            divid.push(veletlendiv);
+            var hely = document.getElementById(veletlendiv);
+        }
+
+        /*
+        do
+        {
+            veletlendiv = Math.floor(Math.random()*(29-0+1)+0);
+        }while(VaneBenne(divid, veletlendiv))
+
+        do
+        {
+            velkartyaszam = Math.floor(Math.random()*(23-1+1)+1);
+        }while(VaneBenne(kartyaszam, velkartyaszam))
+        */
+        /*
+        kartyaszam.push(velkartyaszam);
+        var kep1 = document.createElement("img");
+        kep1.src = "img/"+velkartyaszam+".png";
+        divid.push(veletlendiv);
+        var hely = document.getElementById(veletlendiv);*/
+        hely.appendChild(kep1);
+        
+    }
+    //hazi: dizajn - kepek,divek - ne legyen ismetlodes(helyek, szamok)
+    console.log(kartyaszam);
+    console.log(divid);
+    
+    
+}
+
+function VaneBenne(t,a){
+    var j = 0;
+    while(j<t.lenght && t[j]!=a)
+        j++;
+    if(j<t.lenght)
+        return true;
+}
+
+function Main(){
+    
+    JatekTerBetoltese();
+    JatekTerElrendezese();
+    TablaGeneralasa();
+    KartyatTablabageneral(23);
+}
+
+Main();
