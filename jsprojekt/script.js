6f32f7afdfd7f39a68057452b5839df6209a66bd
Farkas Bence <farkas.b@ckik.hu>
2023-02-21 12:32:18 +0100

    varadatok es kartyaadatok feldolgozasa

--- jsprojekt/script.js
+++ jsprojekt/script.js
@@ -1,4 +1,4 @@
-var kartyAdatok= [
+var kartyAdatok = [
     {id:1,value:1,sign:''},
     {id:2,value:3,sign:''},
     {id:3,value:0,sign:'hegy'},
@@ -23,6 +23,25 @@ var kartyAdatok= [
     {id:22,value:-2,sign:''},
     {id:23,value:-1,sign:''},
 
+];
+
+var varAdatok = [
+    {id:1,color:1,value:1},
+    {id:2,color:1,value:2},
+    {id:3,color:1,value:3},
+    {id:4,color:1,value:4},
+    {id:5,color:2,value:1},
+    {id:6,color:2,value:2},
+    {id:7,color:2,value:3},
+    {id:8,color:2,value:4},
+    {id:9,color:3,value:1},
+    {id:10,color:3,value:2},
+    {id:11,color:3,value:3},
+    {id:12,color:3,value:4},
+    {id:13,color:4,value:1},
+    {id:14,color:4,value:2},
+    {id:15,color:4,value:3},
+    {id:16,color:4,value:4},
 ];
 
 var jatekTer = document.getElementById("jatekter");
@@ -76,5 +95,3 @@ function Main()
 }
 
 Main();
-//console.log(kartyaszam);
-//console.log(divid);
