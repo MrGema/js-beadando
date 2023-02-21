23d59bd435adef1a9138d043b83536bc6cbb985e
Farkas Bence <farkas.b@ckik.hu>
2023-02-21 12:09:20 +0100

    kepek

--- jsprojekt/script.js
+++ jsprojekt/script.js
@@ -1,19 +1,28 @@
-/*var objektum = {
-    szam1: 34,
-    szam2: 23,
-    id: 0,
-    ertek: "Zöldvár4",
-    Torol: Torol(),
-}
-
-function Torol(){
-    
-    return console.log("töröl");
-    
-}
-
-console.log(objektum.szam1);
-call(objektum.Torol);*/
+var kartyAdatok= [
+    {id:1,value:-3,sign:''},
+    {id:2,value:2,sign:''},
+    {id:3,value:5,sign:''},
+    {id:4,value:3,sign:''},
+    {id:6,value:-6,sign:''},
+    {id:5,value:3,sign:''},
+    {id:7,value:-6,sign:''},
+    {id:8,value:0,sign:'taliga'},
+    {id:9,value:0,sign:'taliga'},
+    {id:10,value:2,sign:''},
+    {id:11,value:0,sign:'hegy'},
+    {id:12,value:-5,sign:''},
+    {id:13,value:4,sign:''},
+    {id:14,value:0,sign:'sarkany'},
+    {id:15,value:5,sign:''},
+    {id:16,value:6,sign:''},
+    {id:17,value:-4,sign:''},
+    {id:18,value:1,sign:''},
+    {id:19,value:-2,sign:''},
+    {id:20,value:-2,sign:''},
+    {id:21,value:0,sign:'hegy'},
+    {id:22,value:3,sign:''},
+    {id:23,value:1,sign:''},
+];
 
 var jatekTer = document.getElementById("jatekter");
 var tabla = document.createElement("div");
@@ -56,90 +65,14 @@ function TablaGeneralasa(){
     }
 }
 
-var kartyaszam = new Array();
-var divid = new Array();
-function KartyatTablabageneral(){
-    for (let index = 0; index < 23; index++) {
-        var velkartyaszam = Math.floor(Math.random()*(23-1+1)+1);
-        var veletlendiv = Math.floor(Math.random()*(29-0+1)+0);
-        if(!(divid.includes(veletlendiv)) && !(kartyaszam.includes(velkartyaszam)))
-        {
-            kartyaszam.push(velkartyaszam);
-            var kep1 = document.createElement("img");
-            kep1.src = "img/"+velkartyaszam+".jpg";
-            var hely = document.getElementById(veletlendiv);
-            divid.push(veletlendiv);
-        }
-        else{
-            index--;
-        }
-        hely.appendChild(kep1);
-    }
-    var varakkepek = new Array();
-    var varak = ["S","P","Z","R"];
-    for (let j = 1; j < 8; j++) {
-        var veletlendiv = Math.floor(Math.random()*(29-0+1)+0);
-        var velvarszam = Math.floor(Math.random()*(4-1+1)+1);
-        var varszin = Math.floor(Math.random()*(3-0+1)+0);
-        var a = velvarszam+varak[varszin];
-        //console.log(a);
-        if(!(divid.includes(veletlendiv)) && !(varakkepek.includes(a)))
-        {
-            var kep1 = document.createElement("img");
-            kep1.src = "img/"+velvarszam+varak[varszin]+".png";
-            var hely = document.getElementById(veletlendiv);
-            varakkepek.push(a);
-            divid.push(veletlendiv);
-        }
-        else{
-            j--;
-            
-        }
-        hely.appendChild(kep1);
-        
-    }
-    }
-    
-    function VaneBenne(t,a){
-        var j = 0;
-        while(j<t.lenght && t[j]!=a)
-        {
-            j++;
-        }
-        if(j<t.lenght)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
-function Main(){
-    
+
+function Main()
+{
+
     JatekTerBetoltese();
     JatekTerElrendezese();
     TablaGeneralasa();
-    KartyatTablabageneral();
-    penzosztas(4);
 }
-
-
-function penzosztas(db){
-    let ertekek = new Array(1, 5, 10, 50)
-    for(let i = 0; i < 4; i++){
-        var egysor = document.createElement("div");
-        for(let j = 0; j < db; j++){
-            var erme = document.createElement("img");
-            erme.src="img/"+ertekek[i]+".png";
-            erme.style.width="90px";
-            erme.style.height="90px";
-            egysor.append(erme);
-        }
-        kartyaBox.appendChild(egysor);
-    }
-}
-
 
 Main();
 //console.log(kartyaszam);
